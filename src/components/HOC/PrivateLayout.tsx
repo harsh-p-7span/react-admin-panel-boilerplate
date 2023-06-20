8049ba02a1702f3471238739d8af049b616de3d3
harsh-p-7span <harsh.p@7span.com>
2023-06-20 19:01:10 +0530

    Changed layout CSS

--- src/components/HOC/PrivateLayout.tsx
+++ src/components/HOC/PrivateLayout.tsx
@@ -4,18 +4,20 @@ const PrivateLayout = ({ children }: { children: JSX.Element | JSX.Element[] }) 
     const title = 'Dashboard';
 
     return (
-        <div className="flex h-full">
-            <Sidebar />
+        <div className="app">
+            <div className="app__sidebar">
+                <Sidebar />
+            </div>
 
-            <div className="flex-1 flex flex-col w-full h-full">
+            <div className="app__header">
                 <div className="flex items-center py-2 px-4 shadow relative z-30 bg-white">
                     <p className="breadcrumbs flex items-center capitalize text-gray-500 space-x-2 text-sm p-1 font-medium">
                         {title}
                     </p>
                 </div>
-
-                <div className="flex-1 overflow-auto">{children}</div>
             </div>
+
+            <div className="app__body">{children}</div>
         </div>
     );
 };
